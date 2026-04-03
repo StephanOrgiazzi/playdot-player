e7cb8692d416bd2859cb3517482ca787d40d9908
StephanOrgiazzi <stephan.orgiazzi@gmail.com>
2026-04-03 20:19:09 +0200

    Fix website base urls for dev and Pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stephanorgiazzi.github.io/playdot-player',
-  base: '/playdot-player',
+  base: '/playdot-player/',
+  trailingSlash: 'always',
 });
