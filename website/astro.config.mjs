f6fa5af75e670f33a74888ab8b835a2819b900cf
StephanOrgiazzi <stephan.orgiazzi@gmail.com>
2026-04-04 12:24:10 +0200

    feat: add SEO optimization and social sharing for website

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stephanorgiazzi.github.io/playdot-player',
   base: '/playdot-player/',
   trailingSlash: 'always',
+  integrations: [sitemap()],
 });
